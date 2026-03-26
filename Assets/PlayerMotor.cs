0574b0ccbd52ab01cab0a9f45d13d724df82815d
merrha <merrha.ha@gmail.com>
2026-03-26 09:43:14 +0100

    Update player movement
    łazi jak marian :)

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -6,22 +6,59 @@ public class PlayerMotor : MonoBehaviour
     Vector2 direction;
     private bool canJump = true;
     private Rigidbody2D rigidbody2D;
-    public float speed = 10;
+    public float speed = 5;
     public float jumpForce = 10;
+    public float max_speed = 10;
+    public float stoppingForce = 5; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+       PlayerMove();
+       HandleMaxSpeed();
+       PlayerStopping();
+
+        
+    }
+
+    private void PlayerMove()
+    {
+        rigidbody2D.AddForce(new Vector2(direction.x, 0) * speed);
+    }
+
+    private void HandleMaxSpeed()
+    {
+        if (rigidbody2D.linearVelocityX >= max_speed)
+        {
+
+            rigidbody2D.linearVelocityX = max_speed;
+
+        }
+
+        else if (rigidbody2D.linearVelocityX <= -max_speed)
+        {
+
+            rigidbody2D.linearVelocityX = -max_speed;
+
+        }
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX, 0) * stoppingForce);
+        }
     }
 
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
+
     }
 
 
