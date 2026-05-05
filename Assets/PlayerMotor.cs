0c26144dae819a65e5a2345d1fe324d1a43d384d
merrha <merrha.ha@gmail.com>
2026-05-05 09:50:15 +0200

    jump
    skacze se podwójnie

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -9,7 +9,9 @@ public class PlayerMotor : MonoBehaviour
     public float speed = 5;
     public float jumpForce = 10;
     public float max_speed = 10;
-    public float stoppingForce = 5; 
+    public float stoppingForce = 5;
+    public float maxjump = 2;
+    public float mulltijump = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
@@ -67,13 +69,22 @@ public class PlayerMotor : MonoBehaviour
         if (canJump)
         {
             rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            canJump = false;
+            if (mulltijump > 0)
+            {
+                mulltijump--;
+            }
+            else if (mulltijump == 0)
+            {
+                canJump = false;
+            }
+            
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
+        mulltijump = maxjump;
     }
 }
     
